7f67dac204bdfe83ade8b8125c963e1508f7aa1c
HB10065 <10065@holbertonstudents.com>
2024-12-10 17:15:10 +0000

    quitando redundancias

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,14 +20,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	parent->left = new_node;
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
-		parent->left = new_node;
 	}
-	else
 		parent->left = new_node;
 	return (new_node);
 }
