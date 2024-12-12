a761ff29317c2e7079f71e30c24f19fcb655f0d9
Rodrigo <rodrigoferrer30@gmail.com>
2024-12-12 16:23:40 +0000

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	if (node->parent->right == NULL || node->parent->left == NULL)
